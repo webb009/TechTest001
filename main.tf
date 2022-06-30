c1edc003899c000e81e05023c49cf451e145662a
Rik Webb <rwebb@utopia.io>
2022-06-30 15:15:37 +0100

    added route for nat gateway

--- main.tf
+++ main.tf
@@ -108,3 +108,11 @@ resource "aws_route" "public_internet_gateway" {
         gateway_id              = aws_internet_gateway.internet_gateway.id
 }
 
+# Routing for NAT
+resource "aws_route" "private_nat_gateway" {
+        route_table_id          = aws_route_table.private.id
+        destination_cidr_block  = "0.0.0.0/0"
+        nat_gateway_id          = aws_nat_gateway.nat.id
+}
+
+
