8d0ea15e4e28e8c6fc0752691a93d55629dee9fa
Rik Webb <rwebb@utopia.io>
2022-06-30 15:23:56 +0100

    route table association

--- main.tf
+++ main.tf
@@ -115,4 +115,20 @@ resource "aws_route" "private_nat_gateway" {
         nat_gateway_id          = aws_nat_gateway.nat.id
 }
 
+# Route table assosiation
+
+# Route table association for public subnet
+resource "aws_route_table_association" "public" {
+        count                   = length(var.public_subnets_cidr)
+        subnet_id               = element(aws_subnet.public_subnet.*.id, count.index)
+        route_table_id          = aws_route_table.public.id
+}
+
+# Route table association for private subnet
+resource "aws_route_table_association" "private" {
+        count                   = length(var.private_subnets_cidr)
+        subnet_id               = element(aws_subnet.private_subnet.*.id, count.index)
+        route_table_id          = aws_route_table.private.id
+}
+
 
