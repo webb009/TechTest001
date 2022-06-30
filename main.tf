33ee8e184b63cd7d7c1b956c5a8e98bef076ed0c
Rik Webb <rwebb@utopia.io>
2022-06-30 13:38:01 +0100

    internet gateway added

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
     skip_credentials_validation = true
 }
 
-# Main VPC
+# VPC
 resource "aws_vpc" "vpc" {
     cidr_block                  = var.vpc_cidr
     enable_dns_hostnames        = true
@@ -16,4 +16,12 @@ resource "aws_vpc" "vpc" {
     }
 }
 
-
+# Subnets
+# Internet Gateway
+resource "aws_internet_gateway" "internet_gateway" {
+    vpc_id                      = aws_vpc.vpc.id
+    tags = {
+        Name                    = "${var.environment}-igw"
+        Environemtn             = var.environment
+    }
+}
