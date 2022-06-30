52b705fb6a4cc27a38e640d900043ccaca6f3c11
Rik Webb <rwebb@utopia.io>
2022-06-30 13:48:25 +0100

    NAT added

--- main.tf
+++ main.tf
@@ -25,3 +25,21 @@ resource "aws_internet_gateway" "internet_gateway" {
         Environemtn             = var.environment
     }
 }
+
+# Elastic IP for NAT
+resource "aws_eip" "nat_eip" {
+        vpc                     = true
+        #depends_on              = [aws_internet_gateway.id]
+}
+
+# NAT
+resource "aws_nat_gateway" "nat" {
+        allocation_id           = aws_eip.nat_eip.id
+        subnet_id               = element(aws_subnet.public_subnet.*.id, 0)
+
+        tags = {
+            Name                = "nat"
+            Environment         = "${var.environment}"
+        }
+}
+
