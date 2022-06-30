f1c315ad3f5382e29edd62d4a83f759d4525f5bb
Rik Webb <rwebb@utopia.io>
2022-06-30 14:43:19 +0100

    added public/private subnets

--- main.tf
+++ main.tf
@@ -43,3 +43,32 @@ resource "aws_nat_gateway" "nat" {
         }
 }
 
+# Public Subnet
+resource "aws_subnet" "public_subnet" {
+        vpc_id                  = aws_vpc.vpc.id
+        count                   = length(var.public_subnets_cidr)
+        cidr_block              = element(var.public_subnets_cidr, count.index)
+        availability_zone       = element(var.availability_zones, count.index)
+        map_public_ip_on_launch = true
+
+        tags = {
+            Name                = "$[var.environment}-${element(var.availability_zones, count.index)}-public-subnet"
+            Environment         = "${var.environment}"
+        }
+}
+
+# Private Subnet
+resource "aws_subnet" "private_subnet" {
+        vpc_id                  = aws_vpc.vpc.id
+        count                   = length(var.private_subnets_cidr)
+        cidr_block              = element(var.private_subnets_cidr, count.index)
+        availability_zone       = element(var.availability_zones, count.index)
+        map_public_ip_on_launch = false
+
+        tags = {
+            Name                = "{var.environment}-${element(var.availability_zones, count.index)}-private-subnet"
+            Environment         = "${var.environment}"
+        }
+}
+
+
